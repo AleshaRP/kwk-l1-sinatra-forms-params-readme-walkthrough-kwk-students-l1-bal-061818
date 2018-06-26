a5f97c5044e92de1104d9f48c71c343347aef785
AleshaRP <createlele@yahoo.com>
2018-06-26 20:04:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ class App < Sinatra::Base
   end
 <form>
 <p> Your Name : <input type= "text"> </p>
+<p> Your Favorite Food : <input type= "text"> </p>
+<input type= "submit">
+</form>
   # Add your post route and action below
 
 end
